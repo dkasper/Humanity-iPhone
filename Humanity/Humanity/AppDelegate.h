beb9e6adb45d6e837cf254b42ab471fd1e9c5ca5
David Kasper <dkasper@gmail.com>
2011-11-05 13:12:34 -0700

    xcode project

---
+++ Humanity/Humanity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Humanity
+//
+//  Created by David Kasper on 11/5/11.
+//  Copyright (c) 2011 Yobongo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Humanity/Humanity/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Humanity
+//
+//  Created by David Kasper on 11/5/11.
+//  Copyright (c) 2011 Yobongo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
